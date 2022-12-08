0119b10ed5309eb2e9a2aad086c92326f5084eae
Nkamaime <ninaakpabio@gmail.com>
2022-12-08 15:35:26 -0800

    Must work

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main	-	Entry point of our code
+ * Return:	Return 0 always
+ */
+int	main(0)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
